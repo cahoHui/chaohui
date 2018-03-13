1b8dff819d6c635bb64d78e7d0aa4689bfcaaa0a
张超辉 <zhangchaohui@zhangchaohuideMacBook-Pro.local>
2018-03-13 16:38:46 +0800

    +测试第一个路由配置

--- js/router.js
+++ js/router.js
@@ -1,1 +1,14 @@
-//
+/*
+* data: 2017/08/22
+* author: zhangchaohui
+
+*/
+testproject.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		//js瀑布流
+		.state('/waterfall', {
+			url: '/waterfall',
+			templateUrl: 'tpls/waterfall.html',
+			controller: waterfallController
+		})
+}])
